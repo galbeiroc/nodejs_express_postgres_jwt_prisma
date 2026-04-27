0d70554b88a875068a6b395b90f9d7a3cedb9500
galbeiroc <alcresgu_1989@hotmail.com>
2026-04-26 20:49:24 -0500

    create user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import bcrypt from "bcrypt";
 
 import { prisma } from "../config/db";
 
@@ -19,6 +20,30 @@ const register = async (req: Request, res: Response) => {
   if (userExists) {
     return res.status(400).json({ message: "User already exists" });
   }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return res.status(201).json({
+    status: "success",
+    data: {
+      user: {
+        id: user.id,
+        name,
+        email,
+      },
+    },
+  });
 };
 
 export { register };
