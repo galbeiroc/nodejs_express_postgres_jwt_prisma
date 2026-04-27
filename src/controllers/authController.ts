e9b49fe04727e80b71a5b05fa942c02ca4bc56d3
galbeiroc <alcresgu_1989@hotmail.com>
2026-04-26 22:36:00 -0500

    login user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,14 +3,16 @@ import bcrypt from "bcrypt";
 
 import { prisma } from "../config/db";
 
-interface IRegister {
+type IUser = {
   name: string;
   email: string;
   password: string;
-}
+};
+
+type ILoginUser = Omit<IUser, "name">;
 
 const register = async (req: Request, res: Response) => {
-  const { name, email, password } = req.body as IRegister;
+  const { name, email, password } = req.body as IUser;
 
   // Check if the user already exists
   const userExists = await prisma.user.findUnique({
@@ -46,4 +48,40 @@ const register = async (req: Request, res: Response) => {
   });
 };
 
-export { register };
+const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body as ILoginUser;
+
+  // Check if user email exists in the table
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid email or password",
+    });
+  }
+
+  // Verify password
+  const isValidPassword = await bcrypt.compare(password, user.password);
+
+  if (!isValidPassword) {
+    return res.status(401).json({
+      message: "Invalid email or password",
+    });
+  }
+
+  return res.status(201).json({
+    status: "success",
+    data: {
+      user: {
+        id: user.id,
+        email,
+      },
+    },
+  });
+};
+
+export { login, register };
